640f8f978ac6ea35f524c8750c70118992fe666e
sunw.moon <msun3738@naver.com>
2025-07-24 14:13:30 +0900

    Lombok Dependency 이슈 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,12 @@ repositories {
 
 dependencies {
     implementation("com.google.guava:guava:33.4.8-jre")
-    implementation("org.projectlombok:lombok:1.18.38")
     implementation("org.apache.commons:commons-lang3:3.18.0")
-    
+
+    implementation("org.projectlombok:lombok:1.18.38")
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.mockito:mockito-core:5.18.0")
