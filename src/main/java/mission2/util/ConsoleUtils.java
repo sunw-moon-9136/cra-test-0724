0965441146dbd136f3c7912d1bb0d47127d531b7
sunw.moon <msun3738@naver.com>
2025-07-24 16:17:15 +0900

    테스트코드추가

--- src/main/java/mission2/util/ConsoleUtils.java
+++ src/main/java/mission2/util/ConsoleUtils.java
@@ -1,27 +1,28 @@
 package mission2.util;
 
 import com.google.common.annotations.VisibleForTesting;
+import lombok.AccessLevel;
 import lombok.Setter;
 
 import java.util.Scanner;
 
 public class ConsoleUtils {
-    @VisibleForTesting
-    @Setter
-    private static final ConsoleUtils CONSOLE_UTILS_INSTANCE = new ConsoleUtils();
+    private static final ConsoleUtils consoleUtils = new ConsoleUtils();
 
     public static ConsoleUtils getInstance() {
-        return CONSOLE_UTILS_INSTANCE;
+        return consoleUtils;
     }
 
-    private static final Scanner SCANNER = new Scanner(System.in);
+    @VisibleForTesting
+    @Setter(AccessLevel.PACKAGE)
+    private Scanner scanner = new Scanner(System.in);
 
     public String getInput() {
         System.out.print("INPUT > ");
-        return SCANNER.nextLine().trim();
+        return scanner.nextLine().trim();
     }
 
     public void closeScanner() {
-        SCANNER.close();
+        scanner.close();
     }
 }
--- src/test/java/mission2/util/ConsoleUtilsTest.java
+++ src/test/java/mission2/util/ConsoleUtilsTest.java
@@ -1,16 +1,38 @@
 package mission2.util;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 class ConsoleUtilsTest {
 
-    @Mock
-    Scanner scanner;
+    public static final String NOT_IMPORTANT_TEXT = "test-input";
+    ConsoleUtils consoleUtils = ConsoleUtils.getInstance();
 
-    
+    @AfterEach
+    void clear() {
+        System.setIn(System.in);
+    }
+
+    @Test
+    void getInputTest() {
+        //given
+        InputStream inputStream = new ByteArrayInputStream(NOT_IMPORTANT_TEXT.getBytes());
+        System.setIn(inputStream);
+        consoleUtils.setScanner(new Scanner(System.in));
+
+        //when
+        var result = consoleUtils.getInput();
+
+        //then
+        assertThat(result).isEqualTo(NOT_IMPORTANT_TEXT);
+    }
 }
